cab0ce62de8c6693d3c78eb21f75d71eef178c80
Julien Muetton <themouette@gmail.com>
2014-09-04 00:33:30 +0200

    Add sanitizer test.

---
+++ tests/unit/sanitizer.js
@@ -0,0 +1,127 @@
+describe('sanitizer', function () {
+    var assert = require('assert');
+    var createSanitizer = require('../../lib//sanitizer');
+
+    function assertFieldsError(actual, expected, done) {
+        try {
+            assert.ok(actual.fields, 'there is fields errors.');
+            assert.deepEqual(actual.fields, expected);
+            done(null);
+        } catch (failure) {
+            done(failure);
+        }
+    }
+    function assertGlobalError(actual, expected, done) {
+        try {
+            assert.ok(actual.globals, 'there is global errors errors.');
+            assert.deepEqual(actual.globals, expected);
+            done(null);
+        } catch (failure) {
+            done(failure);
+        }
+    }
+
+    describe('#sanitizeNewUser()', function () {
+        var store, sanitizer, data;
+
+        beforeEach(function () {
+            store = {
+                findUserByUsername: function (username, done) {
+                    done(null, username === 'exists' ? {username: 'exists'} : null);
+                }
+            };
+            sanitizer = createSanitizer(store);
+            data = {
+                    username: 'Foo',
+                    password: 'bar',
+                    password_repeat: 'bar',
+                    email: 'foo@bar.ext'
+                };
+        });
+
+        it('should accept valid user', function (done) {
+            sanitizer.sanitizeNewUser(data, function (err, sanitizedUser) {
+                try {
+                    assert.ifError(err);
+                    assert.equal(sanitizedUser.username, data.username, 'username is forwarded');
+                    done();
+                } catch(failure) {
+                    done(failure);
+                }
+            });
+        });
+
+        it('should reject empty username', function (done) {
+            var expected = {username: ['username field is required']};
+            data.username = '';
+
+            sanitizer.sanitizeNewUser(data, function (err, user) {
+                assertFieldsError(err, expected, done);
+            });
+        });
+
+        it('should reject existing username username', function (done) {
+            var expected = {username: ['username is already in use']};
+            data.username = 'exists';
+
+            sanitizer.sanitizeNewUser(data, function (err, user) {
+                assertFieldsError(err, expected, done);
+            });
+        });
+
+        it('should reject empty password', function (done) {
+            var expected = {password: ['password field is required']};
+            data.password = '';
+
+            sanitizer.sanitizeNewUser(data, function (err, user) {
+                assertFieldsError(err, expected, done);
+            });
+        });
+
+        it('should reject mismatch password', function (done) {
+            var expected = ['Passwords does not match.'];
+            data.password = 'aaa';
+
+            sanitizer.sanitizeNewUser(data, function (err, user) {
+                assertGlobalError(err, expected, done);
+            });
+        });
+    });
+
+    describe('#sanitizeNewUser()', function () {
+        var store, sanitizer, data;
+
+        beforeEach(function () {
+            store = {
+                findUserByUsername: function (username, done) {
+                    done(null, username === 'exists' ? {username: 'exists'} : null);
+                }
+            };
+            sanitizer = createSanitizer(store);
+            data = {
+                    email: 'foo@bar.ext'
+                };
+        });
+
+        it('should accept valid profile', function (done) {
+            sanitizer.sanitizeProfileUpdate(data, function (err, sanitizedProfile) {
+                try {
+                    assert.ifError(err);
+                    assert.equal(sanitizedProfile.username, data.username, 'username is forwarded');
+                    done();
+                } catch(failure) {
+                    done(failure);
+                }
+            });
+        });
+
+        it('should reject empty email', function (done) {
+            var expected = {email: ['email field is required']};
+            data.email = '';
+
+            sanitizer.sanitizeProfileUpdate(data, function (err, user) {
+                assertFieldsError(err, expected, done);
+            });
+        });
+    });
+});
