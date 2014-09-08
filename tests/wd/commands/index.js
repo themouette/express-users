e9b41e619f56f2f57d67fbedca74a314047c1c8d
Julien Muetton <themouette@gmail.com>
2014-09-09 01:21:13 +0200

    Fix Travis build

--- tests/wd/commands/index.js
+++ tests/wd/commands/index.js
@@ -2,6 +2,6 @@ var _ = require('lodash');
 
 module.exports = _.extend({},
         require('screenstory/extensions/chai'),
-        require('tests/wd/commands/register'),
-        require('tests/wd/commands/server')
+        require('./register'),
+        require('./server')
     );
